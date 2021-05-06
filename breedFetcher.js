cb729c8551499e8a7356a6493cd92667660929fb
Zio <zi.ao.tan@outlook.com>
2021-05-06 20:27:24 +0000

    finished refactoring and mocha tests

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,21 +1,20 @@
 const request = require("request");
-const breedName = process.argv[2];
-const URL = "https://api.thecatapi.com/v1/breeds/search?q=" + breedName;
 
-request(URL, (error, response, body) => {
-  if (error) {
-    console.log("Request error: ", error);
-  } else {
-    console.log("response: ", response.statusCode);
-    // console.log("body: ", body);
-    // console.log(typeof body);
-    const data = JSON.parse(body);
-    // console.log(data);
-    // console.log(typeof data);
-    if (data.length === 0) {
-      console.log("Did Not find the breed!");
-    } else {
-      console.log(data[0]);
+const fetchBreedDescription = function(breedName, callback) {
+  const URL = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
+  request(URL, (error, response, body) => {
+    if (error) {
+      return callback(error, null);
     }
-  }
-});
+
+    const bodyParsed = JSON.parse(body);
+
+    if (bodyParsed.length === 0) {
+      return callback("Did Not find the breed!");
+    }
+    return callback(null, bodyParsed[0]['description']);
+  });
+};
+
+module.exports = { fetchBreedDescription };
+
